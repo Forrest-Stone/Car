d6b3d978b71058d6526d2c6a7a2da2da3e17dcd7
liwen123cd <liwen123cd@sina.com>
2018-07-30 14:12:39 +0800

    编译已过

--- send_controller.cpp
+++ send_controller.cpp
@@ -15,25 +15,101 @@ Send_List_Item* Send_Controller::Send_Create_Item(const QString &picture)
     return item;
 }
 
-QByteArray Send_Controller::Send_Image_to_Base64(const QString &path)
+QByteArray Send_Controller::Send_Name_to_Base64(const QString &path)
+{
+
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_11);
+
+    block.clear();
+    out.device()->seek(0);
+
+    int count=path.split('/').count()-1;
+    out << 0x01 << path.section('/',count,count);
+
+    return block;
+}
+
+QByteArray Send_Controller::Send_Size_to_Base64(const QString &path)
 {
     QByteArray block;
-    QBuffer buffer;
-       // QString style;
-    QDataStream out(&block,QIODevice::WriteOnly);
-    //out.setVersion(QDataStream::Qt_5_8);
-    //style = pictureString_list[picIndex].right(3);
-    buffer.open(QIODevice::WriteOnly);
-    QPixmap pix;
-    pix.load(path);
-    pix.save(&buffer,"JPG");
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_11);
 
-    //picturelist[picIndex].save(&buffer,style.toStdString().c_str());
-    //picIndex++;
-    qDebug()<<"buffer"<<buffer.data().size();
-    out<<(quint64)buffer.data().size();
-    //out<<style;
-    block.append(buffer.data());
+    QFile file(path);
+    if(!file.open(QIODevice::ReadOnly)) {
+        qDebug()<<path<<"open fail";
+        return NULL;
+    }
+
+    block.clear();
+    out.device()->seek(0);
+    out << 0x02 << QString::number(file.size());
+
+    file.close();
+    return block;
+}
+
+QList<QByteArray> Send_Controller::Send_Image_to_Base64(const QString &path)
+{
+    QList<QByteArray> list;
+    list.clear();
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_11);
+
+    QFile file(path);
+    if(!file.open(QIODevice::ReadOnly)) {
+        qDebug()<<path<<"open fail";
+        return list;
+    }
+
+    do{
+        block.clear();
+        out.device()->seek(0);
+        out << 0x03 << file.read(0xFFFF);
+        list.push_back(block);
+    }while(!file.atEnd());
+
+    file.close();
+    return list;
+}
+
+QByteArray Send_Controller::Send_End_to_Base64(const QString &path)
+{
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_11);
+
+    block.clear();
+    out.device()->seek(0);
+    int count=path.split('/').count()-1;
+    out << 0x04 << path.section('/',count,count);
+
+    return block;
+}
+
+QList<QByteArray> Send_Controller::Send_Picture_to_Base64(const QString &path, const int option)
+{
+    QList<QByteArray> block;
+    block.clear();
+    switch (option) {
+    case 1:
+        block.append(Send_Name_to_Base64(path));
+        break;
+    case 2:
+        block.append(Send_Size_to_Base64(path));
+        break;
+    case 3:
+        block.append(Send_Image_to_Base64(path));
+        break;
+    case 4:
+        block.append(Send_End_to_Base64(path));
+        break;
+    default:
+        break;
+    }
     return block;
 }
 
